d81f4a5452f9814a46a7fb441687c8d25df405f4
MingGeng <gengming8859@icloud.com>
2019-02-19 11:35:39 +0000

    down file sources

--- 2_3_pillow/test_pillow.py
+++ 2_3_pillow/test_pillow.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+
 from PIL import Image
 
 im = Image.new("RGB", (128,128),"red")
 im.save("red.jpg")
 
 
+im_copy = im.resize((256,256),resample=0,box=None)
 
+im_copy.save('red_resize.jpg')
